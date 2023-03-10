7c91a89c36bebc6247c0a9d9ad8c5fc94c8e4d1a
Rafael Malanco <rafael.malanco@softtek.com>
2023-03-10 15:12:02 -0600

    ok

--- helpers/utils.php
+++ helpers/utils.php
@@ -125,27 +125,26 @@ class Utils
     public static function encryptData($data)
     {
         $key = hash('sha256', SECRET_KEY);
-        $ivlen = openssl_cipher_iv_length($cipher = "AES-128-CBC");
-        $iv = openssl_random_pseudo_bytes($ivlen);
-        $ciphertext_raw = openssl_encrypt($data, $cipher, $key, $options = OPENSSL_RAW_DATA, $iv);
-        $hmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary = true);
-        $ciphertext = base64_encode($iv . $hmac . $ciphertext_raw);
-        return $ciphertext;
+        $ivLen = openssl_cipher_iv_length($cipher = "AES-128-CBC");
+        $iv = openssl_random_pseudo_bytes($ivLen);
+        $cipherTextRaw = openssl_encrypt($data, $cipher, $key, OPENSSL_RAW_DATA, $iv);
+        $hmac = hash_hmac('sha256', $cipherTextRaw, $key, true);
+        $cipherText = base64_encode($iv . $hmac . $cipherTextRaw);
+        return $cipherText;
     }
 
     public static function decryptData($ciphertext)
     {
         $key = hash('sha256', SECRET_KEY);
         $c = base64_decode($ciphertext);
-        $ivlen = openssl_cipher_iv_length($cipher = "AES-128-CBC");
-        $iv = substr($c, 0, $ivlen);
-        $hmac = substr($c, $ivlen, $sha2len = 32);
-        $ciphertext_raw = substr($c, $ivlen + $sha2len);
-        $original_plaintext = openssl_decrypt($ciphertext_raw, $cipher, $key, $options = OPENSSL_RAW_DATA, $iv);
-        $calcmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary = true);
-        if (hash_equals($hmac, $calcmac)) //PHP 5.6+ timing attack safe comparison
-        {
-            return $original_plaintext;
+        $ivLen = openssl_cipher_iv_length($cipher = "AES-128-CBC");
+        $iv = substr($c, 0, $ivLen);
+        $hmac = substr($c, $ivLen, $sha2len = 32);
+        $cipherTextRaw = substr($c, $ivLen + $sha2len);
+        $originalPlaintext = openssl_decrypt($cipherTextRaw, $cipher, $key, OPENSSL_RAW_DATA, $iv);
+        $calcMac = hash_hmac('sha256', $cipherTextRaw, $key, true);
+        if (hash_equals($hmac, $calcMac)) {
+            return $originalPlaintext;
         }
         return null;
     }
